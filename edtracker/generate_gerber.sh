40346398fb5e5685273a59ae6737d07799b48114
FalcoGer <16250196+FalcoGer@users.noreply.github.com>
2021-01-23 03:30:22 +0100

    added GerberRender submodule updated images using the renderer

--- edtracker/generate_gerber.sh
+++ edtracker/generate_gerber.sh
@@ -2,4 +2,16 @@
 
 python3 ./generate_gerber.py edtracker
 zip edtracker-gerber.zip *.gbr *.gbrjob *.drl
+../GerberRender/gerbrend \
+    -c gold \
+    -i ../front.png \
+    -j ../back.png \
+    -f ./edtracker-F_Cu.gbr \
+    -m ./edtracker-F_Mask.gbr \
+    -s ./edtracker-F_SilkS.gbr \
+    -b ./edtracker-B_Cu.gbr \
+    -n ./edtracker-B_Mask.gbr \
+    -t ./edtracker-B_SilkS.gbr \
+    -d ./edtracker-PTH.drl \
+    -o ./edtracker-Edge_Cuts.gbr
 
